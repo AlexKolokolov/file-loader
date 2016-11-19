b06270955fed9e8e49836c8b62985f48613a7adf
kolokolov <kolokolov_a@ukr.net>
2016-11-19 14:52:54 +0200

    Add download speed limiting to DownloadService
    Now download speed is limited using the Tocken
    Bucket algorithm.

--- src/main/java/org/kolokolov/fileloader/service/DownloadService.java
+++ src/main/java/org/kolokolov/fileloader/service/DownloadService.java
@@ -1,7 +1,11 @@
 package org.kolokolov.fileloader.service;
 
+import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.URL;
 import java.util.concurrent.Future;
 
@@ -9,14 +13,24 @@ import org.apache.commons.io.FileUtils;
 
 public class DownloadService {
     
-    private ThreadService threadService;
+    private int bufferSize = 1024;
+    private int speedLimit;
     
-    public DownloadService() {
+    private ThreadService threadService;
+    private TockenBuket tockenBuket;
+    
+    public DownloadService(int speedLimit) {
         threadService = new ThreadService();
+        this.tockenBuket = new TockenBuket();
+        this.speedLimit = speedLimit;
+        this.tockenBuket.fillBucket(this.speedLimit);
     }
     
-    public DownloadService(ThreadService threadService) {
+    public DownloadService(ThreadService threadService, int speedLimit) {
         this.threadService = threadService;
+        this.tockenBuket = new TockenBuket();
+        this.speedLimit = speedLimit;
+        this.tockenBuket.fillBucket(this.speedLimit);
     }
     
     public Future<Boolean> downloadFileInNewThread(URL url, File file) {
@@ -27,24 +41,75 @@ public class DownloadService {
         synchronized (this) {
             System.out.printf("File %s downloading started%n", file.getName());
         }
-        try {
+        try (InputStream is = url.openStream();
+                BufferedOutputStream bos = new BufferedOutputStream(new FileOutputStream(file))){
             if (!file.exists()) file.createNewFile();
             long startTime = System.currentTimeMillis();
-            FileUtils.copyURLToFile(url, file, 3000, 3000);
+            copyBytsIfAllowed(is, bos);
             long downloadTime = System.currentTimeMillis() - startTime;
             long byteSize = FileUtils.sizeOf(file);
-            long downlodSpeed = 1000 * byteSize / downloadTime;
+            long downlodSpeed = 8 * byteSize * 1000 / downloadTime / 1024;
             String fileSize = FileUtils.byteCountToDisplaySize(byteSize);
             synchronized (this) {
-                System.out.printf("File %s : %s : %d bit/s has been downloaded%n", file.getName(), fileSize, downlodSpeed);
+                System.out.printf("File %s : %s has been downloaded at %d kbit/s%n", file.getName(), fileSize, downlodSpeed);
             }
             return true;
-        } catch (IOException ioe) {
+        } catch (IOException | InterruptedException e) {
             synchronized (this) {
                 System.out.printf("File %s downloading error%n", file.getName());
-                System.out.printf("Error message: %s%n", ioe.getMessage());
+                System.out.printf("Error message: %s%n", e.getMessage());
             }
             return false;
+        }
+    }
+    
+    public void copyBytsIfAllowed(InputStream source, OutputStream target) throws IOException, InterruptedException {
+        int count;
+        byte[] buffer = new byte[bufferSize];
+        while ((count = source.read(buffer)) != -1) {
+            synchronized (tockenBuket) {
+                while (tockenBuket.bucket < count) {
+                    tockenBuket.wait();
+                }
+                tockenBuket.bucket -= count;
+                tockenBuket.notifyAll();
+            }
+            target.write(buffer, 0, count);
+        }
+        target.flush();
+    }
+    
+    private class TockenBuket {
+        private int bucket;
+        private int delay = 5; //ms
+        
+        public void fillBucket(int speed) {
+            int stepSize = 1024 * speed * delay / 1000 / 8;
+            int limit = bufferSize > stepSize ? bufferSize * 2 : stepSize * 2;
+            bucket = limit;
+
+            System.out.printf("Speed limit = %d kbit/s%n", speed);
+            System.out.printf("Delay = %d ms%n", delay);
+            System.out.printf("Step size = %d B%n", stepSize);
+            Thread bucketFiller = new Thread(() -> {
+                while (true) {
+                    try {
+                        synchronized (this) {
+                            while (bucket >= limit) {
+                                wait();
+                            }
+                            bucket += stepSize;
+                            Thread.sleep(delay);
+                            notifyAll();
+
+                        }
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            bucketFiller.setDaemon(true);
+            bucketFiller.start();
         }
     }
 }
